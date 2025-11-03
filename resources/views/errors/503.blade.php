a2701c53ded665cfac3bed447a5c4abbf49a05dd
rei-soemanto <reresoemanto@gmail.com>
2025-11-03 13:51:46 +0700

    error page update 2

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <main class="product-main-background" style="background-image: url('{{ asset('img/logoWKM.jpg') }}'); background-size: 70%;">
     <div class="hero-container d-flex flex-column">
-        <div class="bg-overlay min-vh-100 py-5 flex-grow-1 d-flex align-items-center justify-content-center text-center">
+        <div class="bg-overlay min-vh-60 py-5 flex-grow-1 d-flex align-items-center justify-content-center text-center">
             
             <div>
                 <h1 class="display-huge fw-bolder text-gold">503</h1>
