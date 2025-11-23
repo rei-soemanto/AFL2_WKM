35de3a8408c7660c33257e28fc3f0d221f6ef34c
rei-soemanto <reresoemanto@gmail.com>
2025-11-23 16:17:09 +0700

    Registration door error fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -49,10 +50,15 @@ class RegisterController extends Controller
     // Create new user after registration
     protected function create(array $data)
     {
+        $userRole = UserRole::where('name', 'User')->first();
+
+        $roleId = $userRole ? $userRole->id : 3;
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role_id' => $roleId,
         ]);
     }
 
