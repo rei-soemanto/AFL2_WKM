409e1bdc992f6ff1988fe2c51067f1564ce71685
rei-soemanto <reresoemanto@gmail.com>
2025-11-23 14:31:45 +0700

    deleted all admin features

--- resources/views/layout/admin_footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="copyright">
-    <div class="container-fluid px-0 py-0">
-        <p class="footer-copyright 
-            text-center text-white-50 
-            w-full py-4 mx-0 my-0">
-            © 2025 Wraksa Kencana Mukti. All rights reserved.
-        </p>
-    </div>
-</div>
